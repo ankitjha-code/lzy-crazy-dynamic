c42d736ee1a28d8362b19b25e1c47c5e4d86c4d9
prem9084 <146803260+prem9084@users.noreply.github.com>
2025-05-27 12:35:54 +0530

    my updates on Prem branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import About from "./components/About/About";
-import Banner from "./components/Banner/Banner";
 
 const App = () => {
   return (
     <div>
-      <Banner />
       <About />
     </div>
   );
