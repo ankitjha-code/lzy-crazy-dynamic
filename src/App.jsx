1e3124b987090990e9da53c8ecf293816da3b86e
Ajeetyadav12 <ajeetyadav25052001@gmail.com>
2025-05-27 12:24:16 +0530

    navbar and footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold">Welcome to Vite + React</h1>
-      <p className="mt-4 text-lg">
-        Get started by editing <code>src/App.jsx</code>
-      </p>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </>
   );
 };
 
