d0e468133f762d2bb628140300c681d7d33163e2
Anjali04040 <anjali04042018@gmail.com>
2025-05-27 14:41:47 +0530

    Blog page and Appointment Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import BlogPage from "./components/BlogPage";
+import AppointmentPage from "./components/AppointmentPage";
 
 const App = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold">Welcome to Vite + React</h1>
-      <p className="mt-4 text-lg">
-        Get started by editing <code>src/App.jsx</code>
-      </p>
+    <div className="">
+      <BlogPage/>
+      <AppointmentPage/>
     </div>
   );
 };
